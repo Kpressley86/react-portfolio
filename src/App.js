169eae521719f906cdce942895c56f800ac590ed
Kelly Pressley <kellypressley86@gmail.com>
2020-01-03 22:05:04 -0700

    working on side nav bar and header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './orb.gif';
+import logo from './images/orb.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header">       
+        <h2 className="name">KELLY
         <img src={logo} className="App-logo" alt="logo" />
+          PRESSLEY
+        </h2>
       </header>
     </div>
   );
