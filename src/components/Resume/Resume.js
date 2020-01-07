d3bc08ce26ca98633883b464a90e76c64ea5f85d
Kelly Pressley <kellypressley86@gmail.com>
2020-01-06 21:49:31 -0700

    added sideNav working on styling issues and trying to implement PDF for resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -4,80 +4,8 @@ import './Resume.css';
 const Resume = () => {
     return (
       <div className="Resume">
-          <div id="resume" class="container text-center">
-            <div class="resume-container">
-                <div class="row">
-                    <h1>Kelly Pressley
-                        <br>
-                        <br>
-                        Gilbert, AZ 85296
-                        Phone: 541-510-5914 | Email: KellyPressley86@gmail.com <br>
-                        LinkedIn: https://www.linkedin.com/in/kelly-pressley-5490b3184/
-                        <br>
-                        Github: https://github.com/Kpressley86?tab=repositories
-                        <br>
-                        Portfolio: https://kpressley86.github.io/Portfolio/
-                        <br>
-                        <br>
-                        Summary
-                        <br>
-                        <br>
-                        [Summary section can use either bullet point format or paragraph format to sum up your current
-                        title, accomplishments, relevant past experience, recognitions, and/or roles in projects built]
-                        Example: Full Stack Web Developer with a background in Psychology and life-long dedication to
-                        learning. Effective at combining creativity and problem solving to develop user-friendly
-                        applications. Known among staff for strong wit and attention to detail no matter the complexity
-                        of the project.
-                        <br>
-                        <br>
-                        Technical Skills
-                        <br>
-                        <br>
-                        Languages: JavaScript
-                        <br>
-                        Front-end: HTML5, CSS, Bootstrap, Mesmerize.
-                        <br>
-                        Back-end: Node.js,
-                        <br>
-                        <br>
-                        Projects
-                        <br>
-                        <br>
-                        [Repeat this format for 3-4 projects that are ready to be employer-facing] <br>
-                        <br>
-                        Project Name | Link to Github | Link to Deployed Project [always write out link address]
-                        Short summary of project (1-2 lines)<br>
-                        Your core responsibilities in project (1-2 lines)<br>
-                        Tools / languages used<br>
-                        <br>
-                        <br>
-                        Experience
-                        <br>
-                        <br>
-                        Title of your role as an Intern/Volunteer/Assistant/Employee Year – Year<br>
-                        Company Name City, ST<br>
-                        Add a brief description here of your responsibilities; utilize transferable skills (e.g.
-                        communication, critical thinking) to correlate with role of interest. Use past or present verbs
-                        to begin sentences (be consistent with verb tense for each role)
-                        Key Accomplishments:<br>
-                        If applicable, bullet-point 2-4 key value-adds or recognitions, i.e., awards, nominations,
-                        special assignments, leadership, praise, etc.
-                        <br>
-                        <br>
-                        Education
-                        <br>
-                        <br>
-                        Bootcamp Certificate:University, City, ST [Bootcamp should be listed first]<br>
-                        A 24-week intensive program focused on gaining technical programming skills in HTML5, CSS3,
-                        Javascript, JQuery, Bootstrap, Firebase, Node Js, MySQL, MongoDB, Express, Handelbars.js &
-                        ReactJS. Program Summary optional
-                        <br>
-                        <br>
-                        Certification: UofA Coding Bootcamp, Gilbert, AZ
-                    </h1>
-                </div>
-            </div>
-        </div>
+        RESUME!
+         
       </div>
     );
   }
