8e75a785674a6413c3cb23d79924a174dfe4c634
Kelly Pressley <kellypressley86@gmail.com>
2020-02-19 12:03:22 -0700

    changed about me intro

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -16,9 +16,9 @@ const About = () => {
             <div>
                 <div>
                     <h3>
-                        Hi, my name is Kelly Pressley. I am curently attending a Full-Stack Bootcamp at the University
+                        Hi, my name is Kelly Pressley. I recently finished a Full-Stack Bootcamp at the University
                         of Arizon. I'm pretty fluent in JavaScript whether its DOM manipulation or making API calls. I built 
-                        this website using React and a lot of help from Google.
+                        this website using React and a little help from Google.
                         Please feel free to look at my <a href="#Portfolio">Portfolio </a> for some of my recent projects. 
                         <a href="#Contact"> Contact</a> me if you have any questions or comments. Also my GitHub, LinkedIn, and
                         StackOverflow are other ways to conect with me. Links are at the bottom of the page.
